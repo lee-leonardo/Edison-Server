84ae84c7ecb1595c2f9b2a755c16c412f3414eb1
Leonardo Lee <lee.leonardo.king@gmail.com>
2015-02-21 14:37:32 -0800

    Initial commit, basic stuff with edison

---
+++ server.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+var path = require('path');
+var five = require("johnny-five");
+var Edison = require("edison-io");
+
+//Copied from the edison-io npm page.
+var board = new five.board({
+  io: new Edison()
+});
+
+board.on("ready", function () {
+  var led = new five.Led(13);
+  led.blink();
+});
